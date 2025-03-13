a5aa7b6d7a7c8b807905a501e35f94d8a57c8a2c
MVWerly <mv.werly2@gmail.com>
2025-03-13 10:44:21 -0300

    configurando jest e criando testes no header

---
+++ src/utils/tests.tsx
@@ -0,0 +1,33 @@
+import { PropsWithChildren } from 'react'
+import { Provider } from 'react-redux'
+
+import { PreloadedState } from '@reduxjs/toolkit'
+import { render, RenderOptions } from '@testing-library/react'
+
+import { RootState, AppStore, configuraStore } from '../store'
+
+interface ExtendedRenderOptions extends Omit<RenderOptions, 'queries'> {
+  preloadedState?: PreloadedState<RootState>
+  store?: AppStore
+}
+
+export function renderizaComProvider(
+  elemento: React.ReactElement,
+  {
+    preloadedState = {},
+    store = configuraStore(preloadedState),
+    ...opcoesAdicionais
+  }: ExtendedRenderOptions = {}
+) {
+  function Encapsulador({ children }: PropsWithChildren<object>): JSX.Element {
+    return <Provider store={store}>{children}</Provider>
+  }
+
+  return {
+    store,
+    ...render(elemento, {
+      wrapper: Encapsulador,
+      ...opcoesAdicionais
+    })
+  }
+}
